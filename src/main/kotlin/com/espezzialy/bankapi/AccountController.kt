01d88688dd851ed1c2c89fed099615dbb343e1dd
Espezzialy Souza <espezzialy@ciandt.com>
2021-06-15 10:46:54 -0300

    impl: Adding unit tests for methods in controller

--- src/main/kotlin/com/espezzialy/bankapi/AccountController.kt
+++ src/main/kotlin/com/espezzialy/bankapi/AccountController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*
 class AccountController(private val repository: AccountRepository) {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody account: Account): Account = repository.save(account)
 
     @GetMapping
