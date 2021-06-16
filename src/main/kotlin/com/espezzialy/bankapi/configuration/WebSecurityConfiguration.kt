a4a64b58da48b227fec606fe7b9d9f197895a397
Espezzialy Souza <espezzialy@ciandt.com>
2021-06-15 23:57:38 -0300

    bugfix: Fix SQL

--- src/main/kotlin/com/espezzialy/bankapi/configuration/WebSecurityConfiguration.kt
+++ src/main/kotlin/com/espezzialy/bankapi/configuration/WebSecurityConfiguration.kt
@@ -1,35 +1,35 @@
-package com.espezzialy.bankapi.configuration
-
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
-
-@Configuration
-@EnableWebSecurity
-class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
-        http.httpBasic()
-    }
-
-    @Bean
-    override fun userDetailsService(): UserDetailsService {
-        val firstUser = User.withDefaultPasswordEncoder()
-            .username("espezzialy")
-            .password("12345")
-            .roles("USER")
-            .build()
-
-        val admin = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("admin")
-            .roles("ADMIN")
-            .build()
-
-        return InMemoryUserDetailsManager(firstUser, admin)
-    }
-}
+//package com.espezzialy.bankapi.configuration
+//
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+//import org.springframework.security.core.userdetails.User
+//import org.springframework.security.core.userdetails.UserDetailsService
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager
+//
+//@Configuration
+//@EnableWebSecurity
+//class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
+//    override fun configure(http: HttpSecurity) {
+//        http.httpBasic()
+//    }
+//
+//    @Bean
+//    override fun userDetailsService(): UserDetailsService {
+//        val firstUser = User.withDefaultPasswordEncoder()
+//            .username("espezzialy")
+//            .password("12345")
+//            .roles("USER")
+//            .build()
+//
+//        val admin = User.withDefaultPasswordEncoder()
+//            .username("admin")
+//            .password("admin")
+//            .roles("ADMIN")
+//            .build()
+//
+//        return InMemoryUserDetailsManager(firstUser, admin)
+//    }
+//}
--- src/main/kotlin/com/espezzialy/bankapi/repository/AccountRepository.kt
+++ src/main/kotlin/com/espezzialy/bankapi/repository/AccountRepository.kt
@@ -2,7 +2,8 @@ package com.espezzialy.bankapi.repository
 
 import com.espezzialy.bankapi.model.Account
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AccountRepository : JpaRepository<Account, Long> {
-
 }
